1cc7ebf771d8e1d79e2e3a41dbd44799cc167d4d
AASHISH SINGH <aashishsingh2803@gmail.com>
2023-11-11 12:31:01 +0530

    removed unwantd logs

--- lib/chitragupta/json_log_formatter.rb
+++ lib/chitragupta/json_log_formatter.rb
@@ -5,11 +5,7 @@ module Chitragupta
     end
 
     def call(log_level, timestamp, _progname, message)
-      puts("inside caa")
-      # return "inside_call - #{@should_trim_long_string}"
-      final_str = Chitragupta::Util::sanitize_keys(log_level, timestamp, message, @should_trim_long_string)
-      puts("final_str - #{final_str}")
-      return final_str
+      return Chitragupta::Util::sanitize_keys(log_level, timestamp, message, @should_trim_long_string)
     end
   end
 end
