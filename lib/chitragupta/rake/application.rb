1e0833ce3464fb3bb99ab66a9290d919d3b779b7
Pragyash <pragyash@browserstack.com>
2026-08-16 00:03:10 +0530

    Fix Rake task context attribution across executions

--- lib/chitragupta/rake/application.rb
+++ lib/chitragupta/rake/application.rb
@@ -1,6 +1,20 @@
 require 'rake'
 module Rake
   class Application
-    attr_accessor :current_task, :execution_id
+    def current_task
+      Thread.current[:chitragupta_rake_current_task]
+    end
+
+    def current_task=(task)
+      Thread.current[:chitragupta_rake_current_task] = task
+    end
+
+    def execution_id
+      Thread.current[:chitragupta_rake_execution_id]
+    end
+
+    def execution_id=(id)
+      Thread.current[:chitragupta_rake_execution_id] = id
+    end
   end
 end
